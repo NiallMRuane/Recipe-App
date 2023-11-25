5c8a84812a00a2fea04335e26c29bc9aff84f6b4
Niall <niallmruane@gmail.com>
2023-11-25 22:21:42 +0000

    addIngredientToRecipe and askUserToChooseRecipe functions added so ingredients can be added to a recipe

--- src/main/kotlin/controllers/RecipeAPI.kt
+++ src/main/kotlin/controllers/RecipeAPI.kt
@@ -10,12 +10,17 @@ class RecipeAPI {
     private var lastId = 0
     private fun getId() = lastId++
     fun add(recipe: Recipe): Boolean {
+        recipe.recipeId = getId()
         return recipes.add(recipe)
     }
 
     fun listRecipes(): String =
         if (recipes.isEmpty()) "No recipes stored"
         else formatListString(recipes)
+    fun numberOfRecipes() = recipes.size
+    fun findRecipe(recipeId : Int) =  recipes.find{ recipe -> recipe.recipeId == recipeId }
+
+
 
     fun searchByTitle(searchString : String) =
         formatListString(recipes.filter { recipe -> recipe.recipeTitle.contains(searchString, ignoreCase = true)})
@@ -25,7 +30,5 @@ class RecipeAPI {
 
     fun searchByDifficultyLevel(searchString : String) =
         formatListString(recipes.filter { recipe -> recipe.difficultyLevel.contains(searchString, ignoreCase = true)})
-    fun numberOfRecipes() = recipes.size
-
 
 }
