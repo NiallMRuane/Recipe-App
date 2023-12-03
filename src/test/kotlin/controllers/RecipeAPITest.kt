86f3e285708e70bc7caa0184c944b7a0875e105e
Niall <niallmruane@gmail.com>
2023-12-03 13:16:15 +0000

    Function setup added for testing

--- src/test/kotlin/controllers/RecipeAPITest.kt
+++ src/test/kotlin/controllers/RecipeAPITest.kt
@@ -12,11 +12,39 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNull
 class RecipeAPITest {
 
-    private var learnKotlin: Recipe? = null
-    private var summerHoliday: Recipe? = null
-    private var codeApp: Recipe? = null
-    private var testApp: Recipe? = null
-    private var swim: Recipe? = null
+    private var verySmall: Recipe? = null
+    private var somethingFilling: Recipe? = null
+    private var feedsMany: Recipe? = null
+    private var veryTasty: Recipe? = null
+    private var somethingHealthy: Recipe? = null
     private var populatedNotes: RecipeAPI? = RecipeAPI(YAMLSerializer(File("recipe.yaml")))
     private var emptyNotes: RecipeAPI? = RecipeAPI(YAMLSerializer(File("recipe.yaml")))
+
+
+    @BeforeEach
+    fun setup() {
+        verySmall = Recipe(0, "Tacos", 30, "Easy", false, 250, "Niall")
+        somethingFilling = Recipe(1, "Pasta", 15, "Easy", true, 500, "Rob")
+        feedsMany = Recipe(2, "Lasagna", 45, "Hard", false, 1000, "Christina")
+        veryTasty = Recipe(3, "Pizza", 20, "Medium", false, 800, "Amy")
+        somethingHealthy = Recipe(4, "Salad", 10, "Easy", true, 100, "Kieran")
+
+        //adding 5 Note to the notes api
+        populatedNotes!!.add(verySmall!!)
+        populatedNotes!!.add(somethingFilling!!)
+        populatedNotes!!.add(feedsMany!!)
+        populatedNotes!!.add(veryTasty!!)
+        populatedNotes!!.add(somethingHealthy!!)
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
